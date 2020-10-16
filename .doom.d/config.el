b6ae933300859c1ee39ce9e4eb78b47b68b8451d
Alexander Crompton <alexcromp@gmail.com>
2020-10-16 22:49:13 +0200

    Update config files (with Jupyter, org-roam, etc)

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,54 +1,47 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+(setq doom-font (font-spec :family "Hasklig"))
+(setq doom-theme 'doom-laserwave)
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
+(after! org-roam
+        (setq org-roam-directory "~/zett"
+              org-agenda-files (list "~/zett")
+              deft-directory org-roam-directory
+              deft-recursive t
+              org-roam-graph-viewer nil
+              org-ellipsis " ▾▾▾ "
+              org-lowest-priority ?D
+              org-default-priority ?D
+              org-priority-faces '((?A . (:foreground "red" :weight bold))
+                                   (?B . (:foreground "orange"))
+                                   (?C . (:foreground "yellow"))
+                                   (?D . (:foreground "green"))))
+        (setq org-roam-capture-templates
+              '(("d" "default" plain (function org-roam--capture-get-point)
+                     :file-name "%<%Y-%m-%d>-${slug}"
+                     :head "#+TITLE: ${title}\n#+roam_tags: ${TAGS}\n\n"
+                     :unnarrowed t)
+                ("p" "person" plain (function org-roam--capture-get-point)
+                     :file-name "%<%Y-%m-%d>-${slug}"
+                     :head "#+TITLE: ${title}\n#+roam_tags: person ${TAGS}\n\n"
+                     :unnarrowed t))))
 
+(after! avy
+        (setq avy-all-windows t))
 
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
-;; (setq user-full-name "John Doe"
-      ;; user-mail-address "john@doe.com")
+(use-package! org-fancy-priorities
+              :hook (org-mode . org-fancy-priorities-mode)
+              :config (setq org-fancy-priorities-list '("⚑" "⬆" "⬇" "☕")))
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-vibrant)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
+(setq projectile-project-search-path '("~/Documents/GitHub/"))
 
+(setenv "WORKON_HOME" (concat (getenv "CONDA_PREFIX") "/envs"))
 
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
+(setq linum-format "%4d \u2502 ")
+
+(setq ein:notebook-autosave-frequency 10)
+
+(custom-set-faces
+ '(ein:cell-input-area ((t (:background "#042028"))))
+ '(ein:cell-input-prompt ((t (:inherit header-line :background "#002b35" :foreground "#859900" :inverse-video nil :weight bold))))
+ '(ein:cell-output-prompt ((t (:inherit header-line :background "#002b35" :foreground "#dc322f" :inverse-video nil :weight bold)))))
